d2967b284f0f103df57075987cf636af8178884f
wavalava <86676079+wavalava@users.noreply.github.com>
2021-08-30 18:50:58 -0400

    Update CoordsCommand.cpp

--- Horion/Command/Commands/CoordsCommand.cpp
+++ Horion/Command/Commands/CoordsCommand.cpp
@@ -10,6 +10,6 @@ CoordsCommand::~CoordsCommand() {
 bool CoordsCommand::execute(std::vector<std::string>* args) {
 	vec3_t* pos = g_Data.getLocalPlayer()->getPos();
 	float yPos = pos->y - 1.62f;
-	clientMessageF("[%sHorion%s] %sX: %.2f Y: %.2f Z: %.2f", GOLD, WHITE, GREEN, pos->x, yPos /* eye height */, pos->z);
+	clientMessageF("[%sNatsuki%s] %sX: %.2f Y: %.2f Z: %.2f", PINK, PURPLE, GRAY, pos->x, yPos /* eye height */, pos->z);
 	return true;
 }
