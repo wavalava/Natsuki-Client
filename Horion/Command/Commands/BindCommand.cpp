e067164fccc914f26533df7f62e0293d421567f4
wavalava <86676079+wavalava@users.noreply.github.com>
2021-08-30 18:50:07 -0400

    Updated Bind Command (holy shit i need modules)

--- Horion/Command/Commands/BindCommand.cpp
+++ Horion/Command/Commands/BindCommand.cpp
@@ -2,7 +2,7 @@
 
 #include "../../Module/ModuleManager.h"
 
-BindCommand::BindCommand() : IMCCommand("bind", "Binds modules to specific keys", "<module> <key>") {
+BindCommand::BindCommand() : IMCCommand("bind", "Binds modules to different keys", "<module> <key>") {
 	registerAlias("b");
 }
 
@@ -72,7 +72,7 @@ bool BindCommand::execute(std::vector<std::string>* args) {
 		}
 		return true;
 	} else {
-		clientMessageF("%s%s is currently bound to %s", RED, mod->getModuleName(), Utils::getKeybindName(mod->getKeybind()));
+		clientMessageF("%s%s is currently bound to %s", PINK, mod->getModuleName(), Utils::getKeybindName(mod->getKeybind()));
 		return true;
 	}
 	
