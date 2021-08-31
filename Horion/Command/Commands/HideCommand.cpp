2320735b4e303b75a38f82512913509ec0738c99
wavalava <86676079+wavalava@users.noreply.github.com>
2021-08-30 21:05:48 -0400

    Update HideCommand.cpp

--- Horion/Command/Commands/HideCommand.cpp
+++ Horion/Command/Commands/HideCommand.cpp
@@ -9,9 +9,9 @@ HideCommand::~HideCommand() {
 bool HideCommand::execute(std::vector<std::string>* args) {
 	GameData::hide();
 	if (GameData::shouldHide()) {
-		clientMessageF("[%sHorion%s] %sHidden.", GOLD, WHITE, GREEN);
+		clientMessageF("[%sNatsuki%s] %sHidden.", GOLD, WHITE, GREEN);
 	} else {
-		clientMessageF("[%sHorion%s] %sMod is now visible.", GOLD, WHITE, GREEN);
+		clientMessageF("[%sNatsuki%s] %sMod is now visible.", GOLD, WHITE, GREEN);
 	}
 	return true;
 }
